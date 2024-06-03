4a6e69e7e023047f0f5da50bc045649aa768eb06
zhekas <zhekas2003@mail.ru>
2024-06-03 16:29:37 +0300

    Проба 1.2

--- main_files/game.py
+++ main_files/game.py
@@ -10,15 +10,26 @@ class Game:
 
     #показывает методы
 
-    def show_bg(self, surface):
-        pass
+    def show_bg(self, surface): 
         for row in range(Rows):
             for col in range(Cols):
                 if (row + col) % 2 == 0:
                     color = (234, 235, 200) #зелённый
                 else: 
                     color = (119, 154, 88) #тёмно-зелённый
+                    rect = ()
 
                 rect = (col * SQsize, row * SQsize, SQsize, SQsize) #так сказать рисуем квадрат (1 параметр x, потом y, далее длина и ширина)
 
                 pygame.draw.rect(surface, color, rect)
+    
+    def show_pieces(self, surface):
+        for row in range(Rows):
+            for col in range(Cols):
+                if self.board.squere[row][col].has_piece():
+                    piece = self.board.square[row][col].piece
+                    
+                    img = pygame.image.load(piece.texture)
+                    img_center = col * SQsize + SQsize // 2, row * SQsize + SQsize // 2
+                    piece.texture_rect = img.get_rect(center = img_center)
+                    surface.blit(img, piece.texture_rect)
